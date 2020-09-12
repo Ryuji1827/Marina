3eedb475d0bc574fdc760fe03055d296452fb239
ryuji miyahara <ryu_ji18@icloud.com>
2020-09-12 18:20:50 +0900

    add controller,service

---
+++ src/main/java/com/marina/springboot/MarinaAppApplication.java
@@ -0,0 +1,13 @@
+package com.marina.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarinaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marina/springboot/MarinaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marina.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarinaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
