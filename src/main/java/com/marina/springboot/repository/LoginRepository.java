c32bcb8946d53ddaefe3e237a2d92ec595be20da
ryuji miyahara <ryu_ji18@icloud.com>
2020-09-14 23:43:39 +0900

    【Create】Login/Entity,Repositoy,DB/usersTable

---
+++ src/main/java/com/marina/springboot/repository/LoginRepository.java
@@ -0,0 +1,11 @@
+package com.marina.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marina.springboot.User;
+
+@Repository
+public interface LoginRepository extends JpaRepository<User, Integer> {
+
+}
